d201bde903851ae85a0ac76e1155b9f03b58e811
Justin Bantuelle <justin@mudbugmedia.com>
2013-05-19 10:23:56 -0500

    Add Username to Player Sign Up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,26 @@ class ApplicationController < ActionController::Base
 
   before_filter :set_lobby
   before_filter :player_activity
+  before_filter :configure_permitted_parameters, if: :devise_controller?
+
+protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:username, :email, :password) }
+  end
+
+  def devise_parameter_sanitizer
+    if resource_class.is_a?(Player)
+      Player::ParameterSanitizer.new(Player, :player, params)
+    else
+      super # Use the default one
+    end
+  end
 
 private
 
   def set_lobby(in_lobby = false)
-    current_player.update_attribute :lobby, in_lobby
+    current_player.update_attribute(:lobby, in_lobby) if current_player
   end
 
   def player_activity
