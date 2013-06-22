b82434e82a878dc8e3a9b48481eebe6b3f4a97d9
Justin Bantuelle <justin@mudbugmedia.com>
2013-06-22 10:17:00 -0500

    Update to Rails 4 RC2

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Dominion::Application.config.session_store :encrypted_cookie_store, key: '_dominion_session'
+Dominion::Application.config.session_store :cookie_store, key: '_dominion_session'
