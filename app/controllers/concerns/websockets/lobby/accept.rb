3fd786c1dbe0b3d58499393318c9825cead102a7
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-20 11:32:33 -0500

    Remove erroneous socket logic on game start

--- app/controllers/concerns/websockets/lobby/accept.rb
+++ app/controllers/concerns/websockets/lobby/accept.rb
@@ -5,7 +5,6 @@ module Websockets::Lobby::Accept
     PlayerAccepter.accept(game, current_player)
 
     if game.accepted?
-      move_players_to_game(game)
       send_accepted_game(game)
     else
       send_accept_received
@@ -31,11 +30,6 @@ module Websockets::Lobby::Accept
     ApplicationController.lobby[current_player.id].send_data({
       action: 'player_count_error'
     }.to_json)
-  end
-
-  def move_players_to_game(game)
-    game_players = game.players.collect(&:id)
-    ApplicationController.games[game.id] = ApplicationController.lobby.select{ |player_id, socket| game_players.include? player_id }
   end
 
 end
