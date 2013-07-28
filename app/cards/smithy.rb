c93538d154bb78e41a6583ec48453e93a08ad7dc
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 17:07:27 -0500

    Implement Smithy

--- app/cards/smithy.rb
+++ app/cards/smithy.rb
@@ -14,7 +14,21 @@ module Smithy
     [:action]
   end
 
-  def play
-    # +3 Card
+  def play(game)
+    @card_drawer = CardDrawer.new(game.current_player)
+    @card_drawer.draw(3)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Smithy</span> drawing "
+    if game.current_player.player_id == player.id
+      message += @card_drawer.drawn_cards.map{ |card|
+        "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
+      }.join(' ')
+    else
+      message += "#{@card_drawer.drawn_cards.count} card(s)"
+    end
+    message += "."
   end
 end
