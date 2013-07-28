c594167524892f65c2a2397ecd08c2ca91ff104f
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 11:32:57 -0500

    Implement Silver

--- app/cards/silver.rb
+++ app/cards/silver.rb
@@ -14,7 +14,12 @@ module Silver
     [:treasure]
   end
 
-  def play
-    # +2 coin
+  def play(game)
+    game.current_turn.add_coins(2)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Silver</span>."
   end
 end
