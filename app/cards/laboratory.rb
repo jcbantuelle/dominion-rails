8b77411c4d3d8a154182f0eba95d8e894d1b6361
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 12:54:32 -0500

    Implement Laboratory

--- app/cards/laboratory.rb
+++ app/cards/laboratory.rb
@@ -14,8 +14,22 @@ module Laboratory
     [:action]
   end
 
-  def play
-    # +2 cards
-    # +1 action
+  def play(game)
+    game.current_turn.add_actions(1)
+    @card_drawer = CardDrawer.new(game.current_player)
+    @card_drawer.draw(2)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Laboratory</span> drawing "
+    if game.current_player.player_id == player.id
+      message += @card_drawer.drawn_cards.map{ |card|
+        "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
+      }.join(' ')
+    else
+      message += "#{@card_drawer.drawn_cards.count} card(s)"
+    end
+    message += " and getting +1 action."
   end
 end
