dd60c8931667e3729df268fde835826c8f2b84b5
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 11:41:21 -0500

    Implement Festival

--- app/cards/festival.rb
+++ app/cards/festival.rb
@@ -14,9 +14,14 @@ module Festival
     [:action]
   end
 
-  def play
-    # +2 actions
-    # +1 buy
-    # +2 coin
+  def play(game)
+    game.current_turn.add_coins(2)
+    game.current_turn.add_buys(1)
+    game.current_turn.add_actions(2)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Festival</span> getting +2 actions, +1 buy, and +$2."
   end
 end
