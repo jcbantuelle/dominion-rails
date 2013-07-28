99f7ac3472a13908a9e75f3c8aa4df40b628667f
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 17:07:37 -0500

    Implement Village

--- app/cards/village.rb
+++ app/cards/village.rb
@@ -14,8 +14,22 @@ module Village
     [:action]
   end
 
-  def play
-    # +1 Card
-    # +2 Action
+  def play(game)
+    @card_drawer = CardDrawer.new(game.current_player)
+    @card_drawer.draw(1)
+    game.current_turn.add_actions(2)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Village</span> drawing "
+    if game.current_player.player_id == player.id
+      message += @card_drawer.drawn_cards.map{ |card|
+        "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
+      }.join(' ')
+    else
+      message += "#{@card_drawer.drawn_cards.count} card(s)"
+    end
+    message += " and getting +2 actions."
   end
 end
