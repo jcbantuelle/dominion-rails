d44c0a9c934944b96d2e9910502f8a787c53bb07
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 17:07:20 -0500

    Implement Market

--- app/cards/market.rb
+++ app/cards/market.rb
@@ -14,10 +14,24 @@ module Market
     [:action]
   end
 
-  def play
-    # +1 card
-    # +1 action
-    # +1 buy
-    # +1 coin
+  def play(game)
+    @card_drawer = CardDrawer.new(game.current_player)
+    @card_drawer.draw(1)
+    game.current_turn.add_actions(1)
+    game.current_turn.add_buys(1)
+    game.current_turn.add_coins(1)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Market</span> drawing "
+    if game.current_player.player_id == player.id
+      message += @card_drawer.drawn_cards.map{ |card|
+        "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
+      }.join(' ')
+    else
+      message += "#{@card_drawer.drawn_cards.count} card(s)"
+    end
+    message += " and getting +1 action, +1 buy, and +$1."
   end
 end
