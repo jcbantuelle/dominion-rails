1838230bc49b7edd1be934085af59e657a73e2f1
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 11:33:03 -0500

    Implement Gold

--- app/cards/gold.rb
+++ app/cards/gold.rb
@@ -14,7 +14,12 @@ module Gold
     [:treasure]
   end
 
-  def play
-    # +3 coin
+  def play(game)
+    game.current_turn.add_coins(3)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Gold</span>."
   end
 end
