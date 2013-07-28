425d9105313d467f9aed6431e140f4841c490638
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 17:07:44 -0500

    Implement Woodcutter

--- app/cards/woodcutter.rb
+++ app/cards/woodcutter.rb
@@ -14,8 +14,13 @@ module Woodcutter
     [:action]
   end
 
-  def play
-    # +1 Buy
-    # +2 Coin
+  def play(game)
+    game.current_turn.add_coins(2)
+    game.current_turn.add_buys(1)
+  end
+
+  def log(game, player)
+    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
+    message += " a <span class=\"#{type_class}\">Woodcutter</span> getting +1 buy and +$2."
   end
 end
