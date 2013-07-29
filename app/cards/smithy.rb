ee9c526ba1591257e70a0d9d02f849ee3b1955f4
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 20:18:16 -0500

    Refactor Smithy Log Message

--- app/cards/smithy.rb
+++ app/cards/smithy.rb
@@ -20,15 +20,6 @@ module Smithy
   end
 
   def log(game, player)
-    message = game.current_player.player_id == player.id ? 'You play' : "#{game.current_player.username} plays"
-    message += " a <span class=\"#{type_class}\">Smithy</span> drawing "
-    if game.current_player.player_id == player.id
-      message += @card_drawer.drawn_cards.map{ |card|
-        "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
-      }.join(' ')
-    else
-      message += "#{@card_drawer.drawn_cards.count} card(s)"
-    end
-    message += "."
+    Renderer.new.render 'game/log/play_card', { game: game, player: player, card: self, card_drawer: @card_drawer }
   end
 end
