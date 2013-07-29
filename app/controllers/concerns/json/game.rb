0d1fba49c1e4ad960dc6ba554edfd0755475048b
Justin Bantuelle <justin@mudbugmedia.com>
2013-07-28 20:27:03 -0500

    Refactor Out Code Duplication in Game JSON

--- app/controllers/concerns/json/game.rb
+++ app/controllers/concerns/json/game.rb
@@ -3,43 +3,38 @@ module Json::Game
   include Json::Helper, Json::GameLog
 
   def refresh_game_json(game, player)
-    game_player = game.game_player(player.id)
     {
       action: 'refresh',
       kingdom_cards: game_cards(game, 'kingdom'),
-      common_cards: common_cards(game),
+      common_cards: common_cards(game)
+    }.merge(game_area(game, player)).to_json
+  end
+
+  def end_turn_json(game, player)
+    {
+      action: 'end_turn',
+      log: end_turn_log(game, player)
+    }.merge(game_area(game, player)).to_json
+  end
+
+  def play_card_json(game, player, card_player)
+    {
+      action: 'play_card',
+      log: card_player.log(player)
+    }.merge(game_area(game, player)).to_json
+  end
+
+  private
+
+  def game_area(game, player)
+    game_player = game.game_player(player.id)
+    {
       current_turn: game.current_turn,
       deck_count: game_player.deck.count,
       discard_count: game_player.discard.count,
       hand: sorted_hand(game_player),
       my_turn: same_player?(game.current_player.player, player)
-    }.to_json
-  end
-
-  def end_turn_json(game, player)
-    game_player = game.game_player(player.id)
-    {
-      action: 'end_turn',
-      current_turn: game.current_turn,
-      deck_count: game_player.deck.count,
-      discard_count: game_player.discard.count,
-      hand: sorted_hand(game_player),
-      my_turn: same_player?(game.current_player.player, player),
-      log: end_turn_log(game, player)
-    }.to_json
-  end
-
-  def play_card_json(game, player, card_player)
-    game_player = game.game_player(player.id)
-    {
-      action: 'play_card',
-      current_turn: game.current_turn,
-      deck_count: game_player.deck.count,
-      discard_count: game_player.discard.count,
-      hand: sorted_hand(game_player),
-      my_turn: same_player?(game.current_player.player, player),
-      log: card_player.log(player)
-    }.to_json
+    }
   end
 
 end
