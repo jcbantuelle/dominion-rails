97c7bc10ba7642c8a11a5a7dfb8b667709f7b10d
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-11 14:48:14 -0500

    Clean Up Log Message Logic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,10 @@ module ApplicationHelper
   def same_player?(game_player, player)
     game_player.player_id == player.id
   end
+
+  def show_cards(cards)
+    cards.map{ |card|
+      "<span class=\"#{card.type_class}\">#{card.name.titleize}</span>"
+    }.join(' ').html_safe
+  end
 end
