a136decec38248629900898ce65f35ebee935382
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-12 00:18:49 -0500

    Refactor Adventurer for Compatibility with Changes

--- app/cards/adventurer.rb
+++ app/cards/adventurer.rb
@@ -15,55 +15,46 @@ module Adventurer
   end
 
   def play(game)
-    @revealed = []
-    @treasures = []
-
-    reveal_cards(game)
-    discard_revealed
-  end
-
-  def log(game, player)
-    locals = {
-      revealed: {
-        discarded: @revealed,
-        drawn: @treasures
-      }
-    }
-
-    render_play_card game, player, locals
+    reveal(game)
+    discard_revealed(game)
   end
 
   private
 
+  def reveal(game)
+    @revealed = []
+    @treasures = []
+
+    reveal_cards(game)
+    @log_updater.reveal(game.current_player, @revealed, 'deck')
+  end
+
   def reveal_cards(game)
     game.current_player.deck.each do |card|
       break if @treasures.count == 2
+      @revealed << card
       if card.treasure?
         @treasures << card
         card.update_attribute :state, 'hand'
       else
-        @revealed << card
-        card.update_attribute :state, 'reveal'
+        card.update_attribute :state, 'revealed'
       end
     end
 
-    continue_revealing(game)
+    continue_revealing(game) unless reveal_finished?(game)
   end
 
   def continue_revealing(game)
-    unless reveal_finished?
-      shuffle_discard
-      reveal_cards(game)
-    end
+    game.current_player.shuffle_discard_into_deck
+    reveal_cards(game)
   end
 
-  def discard_revealed
-    @revealed.each do |card|
-      card.update_attribute :state, 'discard'
-    end
+  def discard_revealed(game)
+    game.current_player.discard_revealed
+    @log_updater.put(game.current_player, @treasures, 'hand')
   end
 
-  def reveal_finished?
+  def reveal_finished?(game)
     @treasures.count == 2 || game.current_player.discard.count == 0
   end
 
