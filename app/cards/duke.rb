81d4fbdd66a2d1aebaa5fb27522e46f688e13a32
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-17 00:46:58 -0500

    Implement Duke

---
+++ app/cards/duke.rb
@@ -0,0 +1,22 @@
+module Duke
+
+  def starting_count(game)
+    victory_card_count(game)
+  end
+
+  def cost
+    {
+      coin: 5
+    }
+  end
+
+  def type
+    [:victory]
+  end
+
+  def value(deck)
+    duchy = Card.by_name('duchy')
+    deck.select{|card| card.card_id == duchy.id }.count
+  end
+
+end
