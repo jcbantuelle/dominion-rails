50ceaacebed41311c0a49a822a143d95b917ad0c
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-17 00:46:12 -0500

    Implement Potion

---
+++ app/cards/potion.rb
@@ -0,0 +1,24 @@
+module Potion
+
+  def starting_count(game)
+    16
+  end
+
+  def cost
+    {
+      coin: 4
+    }
+  end
+
+  def type
+    [:treasure]
+  end
+
+  def play(game)
+    #game.current_turn.add_coins(3)
+  end
+
+  def log(game, player)
+    render_play_card game, player
+  end
+end
