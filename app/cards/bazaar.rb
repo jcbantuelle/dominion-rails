79da436475ae43e1c5570d895f5b50a606b4eff6
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-17 11:27:07 -0500

    Implement Bazaar

---
+++ app/cards/bazaar.rb
@@ -0,0 +1,25 @@
+module Bazaar
+
+  def starting_count(game)
+    10
+  end
+
+  def cost(game)
+    {
+      coin: 5
+    }
+  end
+
+  def type
+    [:action]
+  end
+
+  def play(game)
+    @card_drawer = CardDrawer.new(game.current_player)
+    @card_drawer.draw(1)
+    game.current_turn.add_actions(2)
+    game.current_turn.add_coins(1)
+    @log_updater.get_from_card(game.current_player, '+2 actions and +$1')
+  end
+
+end
