2fda0c7d2346afc8eb9d912a22deec11c50fcb7a
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-17 13:46:40 -0500

    Refactor Out Cutpurse Discard Logic

--- app/cards/cutpurse.rb
+++ app/cards/cutpurse.rb
@@ -18,16 +18,20 @@ module Cutpurse
     game.current_turn.add_coins(2)
     @log_updater.get_from_card(game.current_player, '+$2')
 
-    copper = Card.by_name 'copper'
+    @copper = Card.by_name 'copper'
     game.game_players.each do |player|
-      unless player.id == game.current_player.id
-        copper_in_hand = player.hand.select{|card| card.card_id == copper.id }
-        if copper_in_hand.count > 0
-          copper_in_hand.first.discard
-          @log_updater.discard(player, [copper_in_hand.first])
-        else
-          @log_updater.reveal(player, player.hand, 'hand')
-        end
+      discard_copper(game, player)
+    end
+  end
+
+  def discard_copper(game, player)
+    unless player.id == game.current_player.id
+      copper_in_hand = player.hand.select{|card| card.card_id == @copper.id }
+      if copper_in_hand.count > 0
+        copper_in_hand.first.discard
+        @log_updater.discard(player, [copper_in_hand.first])
+      else
+        @log_updater.reveal(player, player.hand, 'hand')
       end
     end
   end
