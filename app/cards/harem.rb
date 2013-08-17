68c220f7300d24f42ea1872758b7fa2e596a543c
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-17 00:46:37 -0500

    Implement Harem

---
+++ app/cards/harem.rb
@@ -0,0 +1,25 @@
+module Harem
+
+  def starting_count(game)
+    victory_card_count(game)
+  end
+
+  def cost
+    {
+      coin: 6
+    }
+  end
+
+  def type
+    [:treasure, :victory]
+  end
+
+  def value(deck)
+    2
+  end
+
+  def play(game)
+    game.current_turn.add_coins(2)
+  end
+
+end
