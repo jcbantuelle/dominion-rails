12e57ea82ef7f93e845cd91558da17f1009deecd
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-18 10:42:38 -0500

    Refactor Witch

--- app/cards/witch.rb
+++ app/cards/witch.rb
@@ -18,13 +18,6 @@ module Witch
     @card_drawer = CardDrawer.new(game.current_player)
     @card_drawer.draw(2)
 
-    curse = Card.by_name 'curse'
-    game_card = game.game_cards.by_card_id(curse.id).first
-
-    game.game_players.each do |player|
-      unless player.id == game.current_player.id
-        CardGainer.new(game, player, game_card.id).gain_card('discard')
-      end
-    end
+    give_card_to_players(game, 'curse', 'discard')
   end
 end
