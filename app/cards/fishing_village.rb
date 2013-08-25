1df9391bb21f05410d24822159b15deb9c2d19e3
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-24 23:36:00 -0500

    Fix Typo in Fishing Village

--- app/cards/fishing_village.rb
+++ app/cards/fishing_village.rb
@@ -23,7 +23,7 @@ module FishingVillage
   def duration(game)
     game.current_turn.add_actions(1)
     game.current_turn.add_coins(1)
-    @log_updater.get_from_card(game.current_player, "+1 actions and +$1 from #{self.card_html}")
+    @log_updater.get_from_card(game.current_player, "+1 action and +$1 from #{self.card_html}")
   end
 
 end
