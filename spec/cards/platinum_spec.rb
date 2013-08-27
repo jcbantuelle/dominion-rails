04916f67b0c0422756399914282381e50128b44a
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:06:05 -0500

    Add Platinum Spec

---
+++ spec/cards/platinum_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'Platinum' do
+  let(:card_name) { 'platinum' }
+
+  include_context "play card"
+
+  describe '#play' do
+    it 'updates the game state' do
+      @subject.play_card
+      @turn.reload
+      expect(@turn.phase).to eq('treasure')
+      expect(@turn.coins).to eq(5)
+    end
+  end
+end
