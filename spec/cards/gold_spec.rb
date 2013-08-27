a35e70d738613c5bc05affe698bfcea4ce6895c5
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:06:12 -0500

    Add Gold Spec

---
+++ spec/cards/gold_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+describe 'Gold' do
+  let(:card_name) { 'gold' }
+
+  include_context "play card"
+
+  describe '#play' do
+    it 'updates the game state' do
+      @subject.play_card
+      @turn.reload
+      expect(@turn.phase).to eq('treasure')
+      expect(@turn.coins).to eq(3)
+    end
+  end
+end
