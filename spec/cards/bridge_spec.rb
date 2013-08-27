2f6724b34b036339aa999bf1af275cb298b26038
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:06:35 -0500

    Add Bridge Spec

---
+++ spec/cards/bridge_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Bridge' do
+  let(:card_name) { 'bridge' }
+
+  include_context "play card"
+
+  describe '#play' do
+    it 'updates the game state' do
+      @subject.play_card
+      @turn.reload
+      expect(@turn.coins).to eq(1)
+      expect(@turn.buys).to eq(2)
+      expect(@turn.global_discount).to eq(1)
+    end
+  end
+end
