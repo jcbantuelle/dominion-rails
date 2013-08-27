a2a4b9cd2286fbce4f2ec44474299834cf968eb4
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:06:50 -0500

    Add Bank Spec

---
+++ spec/cards/bank_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'Bank' do
+  let(:card_name) { 'bank' }
+
+  include_context "play card"
+
+  describe '#play' do
+    it 'updates the game state' do
+      2.times do
+        PlayerCard.create game_player: @game_player, card: @card, state: 'play'
+      end
+      @subject.play_card
+      @turn.reload
+      expect(@turn.phase).to eq('treasure')
+      expect(@turn.coins).to eq(3)
+    end
+  end
+end
