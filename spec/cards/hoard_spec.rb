0fdf6f46731559e3380cde5cd7bfc9658c6204ec
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:55:10 -0500

    Add Hoard Spec

---
+++ spec/cards/hoard_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+describe 'Hoard' do
+  let(:card_name) { 'hoard' }
+
+  describe '#play' do
+    include_context 'play card'
+
+    it 'updates the game state' do
+      @subject.play_card
+      @turn.reload
+      expect(@turn.phase).to eq('treasure')
+      expect(@turn.hoards).to eq(1)
+      expect(@turn.coins).to eq(2)
+    end
+  end
+end
