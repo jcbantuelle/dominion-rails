11f63b85be688475e2f4660d59ad2bf18c0943fa
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:47:58 -0500

    Add Harem Spec

---
+++ spec/cards/harem_spec.rb
@@ -0,0 +1,27 @@
+require 'spec_helper'
+
+describe 'Harem' do
+  let(:card_name) { 'harem' }
+
+  describe '#play' do
+
+    include_context 'play card'
+
+    it 'updates the game state' do
+      @subject.play_card
+      @turn.reload
+      expect(@turn.phase).to eq('treasure')
+      expect(@turn.coins).to eq(2)
+    end
+  end
+
+  describe '#value' do
+
+    include_context 'victory card'
+
+    it 'is worth 2 points' do
+      expect(@card.value(nil)).to eq(2)
+    end
+  end
+
+end
