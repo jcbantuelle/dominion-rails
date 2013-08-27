2b4ef490732c3d94bffc541c35eec3dee9877c5a
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 17:23:44 -0500

    Add Great Hall Spec

---
+++ spec/cards/great_hall_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe 'Great Hall' do
+  let(:card_name) { 'great_hall' }
+
+  describe '#play' do
+    include_context 'play card'
+
+    it 'updates the game state' do
+      PlayerCard.create game_player: @game_player, card: @card, state: 'deck'
+      @subject.play_card
+      @turn.reload
+      expect(@turn.actions).to eq(1)
+      expect(@game_player.hand.count).to eq(1)
+    end
+  end
+
+  describe '#value' do
+
+    include_context 'victory card'
+
+    it 'is worth 1 point' do
+      expect(@card.value(nil)).to eq(1)
+    end
+  end
+end
