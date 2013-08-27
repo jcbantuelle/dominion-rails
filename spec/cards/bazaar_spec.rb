822b0728eeefe1ad914206a42e28f3366446fbfa
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 16:06:44 -0500

    Add Bazaar Spec

---
+++ spec/cards/bazaar_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Bazaar' do
+  let(:card_name) { 'bazaar' }
+
+  include_context "play card"
+
+  describe '#play' do
+    it 'updates the game state' do
+      PlayerCard.create game_player: @game_player, card: @card, state: 'deck'
+      @subject.play_card
+      @turn.reload
+      expect(@turn.actions).to eq(2)
+      expect(@turn.coins).to eq(1)
+      expect(@game_player.hand.count).to eq(1)
+    end
+  end
+end
