d2bc60c9e4e80590f2cce79713563069d10f107f
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 22:23:54 -0500

    Add Adventurer Spec

---
+++ spec/cards/adventurer_spec.rb
@@ -0,0 +1,47 @@
+require 'spec_helper'
+
+describe 'Adventurer' do
+  let(:card_name) { 'adventurer' }
+
+  include_context 'play card'
+
+  describe '#play' do
+
+    before(:each) do
+      5.times do |i|
+        PlayerCard.create game_player: @game_player, card: @card, state: 'deck', card_order: i
+      end
+      @treasure = Card.find(Card.create(name: 'copper').id)
+    end
+
+    it 'discards revealed cards' do
+      @subject.play_card
+      expect(@game_player.discard.count).to eq(5)
+    end
+
+    context 'with two treasures in deck' do
+      it 'puts both treasures in hand' do
+        2.times do |i|
+          PlayerCard.create game_player: @game_player, card: @treasure, state: 'deck', card_order: i+5
+        end
+        @subject.play_card
+        expect(@game_player.hand.count).to eq(2)
+      end
+    end
+
+    context 'with one treasure in deck' do
+      it 'puts one treasure in hand' do
+        PlayerCard.create game_player: @game_player, card: @treasure, state: 'deck', card_order: 6
+        @subject.play_card
+        expect(@game_player.hand.count).to eq(1)
+      end
+    end
+
+    context 'with no treasures in deck' do
+      it 'puts no cards in hand' do
+        @subject.play_card
+        expect(@game_player.hand.count).to eq(0)
+      end
+    end
+  end
+end
