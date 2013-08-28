f8eab36eb894038ae306d235aab31d9430512141
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-27 19:21:14 -0500

    Add Smithy Spec

---
+++ spec/cards/smithy_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'Smithy' do
+  let(:card_name) { 'smithy' }
+
+  include_context 'play card'
+
+  describe '#play' do
+    it 'updates the game state' do
+      3.times do
+        PlayerCard.create game_player: @game_player, card: @card, state: 'deck'
+      end
+      @subject.play_card
+      @turn.reload
+      expect(@game_player.hand.count).to eq(3)
+    end
+  end
+end
