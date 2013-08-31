0709f834318b0373df212d937b492c34088b5861
Justin Bantuelle <justin@mudbugmedia.com>
2013-08-30 21:23:26 -0500

    Implement Chapel

--- app/cards/chapel.rb
+++ app/cards/chapel.rb
@@ -14,7 +14,24 @@ module Chapel
     [:action]
   end
 
-  def play
-    # Trash up to 4 cards
+  def play(game)
+    action = TurnAction.create game: game, game_player: game.current_player
+    action.update sent_json: choose_cards_json(action, game.current_player.hand, 4, 'Choose up to 4 cards to trash:')
+
+    WebsocketDataSender.send_game_data(game.current_player.player, game, action.sent_json)
+
+    Thread.new {
+      while !action.finished? do
+        sleep(1)
+        action.reload
+      end
+      trashed_cards = PlayerCard.where(id: action.response.split)
+      CardTrasher.new(trashed_cards).trash('hand')
+      action.destroy
+
+      hand_json = update_hand_json(game, game.current_player.player)
+      WebsocketDataSender.send_game_data(game.current_player.player, game, hand_json)
+      ActiveRecord::Base.clear_active_connections!
+    }
   end
 end
