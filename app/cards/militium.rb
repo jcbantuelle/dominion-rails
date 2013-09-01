740e7d82eedf406a0191673eb9f5ad4531a3ed06
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-01 09:45:32 -0500

    Implement Militia

--- app/cards/militium.rb
+++ app/cards/militium.rb
@@ -14,8 +14,31 @@ module Militium
     [:action, :attack]
   end
 
-  def play
-    # +2 Coin
-    # Each other player discards down to 3 cards
+  def play(game)
+    game.current_turn.add_coins(2)
+    @log_updater.get_from_card(game.current_player, '+$2')
   end
+
+  def attack(game, players)
+    players.each do |player|
+      hand = player.hand
+      if hand.count <= 3
+        @log_updater.custom_message(player, "#{hand.count} cards in hand", 'have')
+      else
+        discard_count = hand.count - 3
+        action = send_choose_cards_prompt(game, player, hand, "Choose #{discard_count} card(s) to discard:", discard_count, discard_count)
+        process_player_response(game, player, action)
+      end
+    end
+  end
+
+  private
+
+  def process_action(game, game_player, action)
+    discarded_cards = PlayerCard.where(id: action.response.split)
+    discarded_cards.update_all state: 'discard'
+    LogUpdater.new(game).discard(game_player, discarded_cards, 'hand')
+    update_player_hand(game, game_player.player)
+  end
+
 end
