81a3f294860821ba03935961fb3b6fa07227832c
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-01 13:33:13 -0500

    Unify GameTrash API with PlayerCard API

--- app/models/game_trash.rb
+++ app/models/game_trash.rb
@@ -1,4 +1,21 @@
 class GameTrash < ActiveRecord::Base
   belongs_to :game
   belongs_to :card
+
+  def name
+    card.name
+  end
+
+  def type_class
+    card.type_class
+  end
+
+  def json
+    {
+      id: id,
+      name: name,
+      type_class: type_class,
+      title: name.titleize
+    }
+  end
 end
