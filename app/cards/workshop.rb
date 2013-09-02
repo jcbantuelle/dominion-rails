e73723b0d94b9a65ba6ba040f3d670547f9481de
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-02 15:43:04 -0500

    Implement Workshop

--- app/cards/workshop.rb
+++ app/cards/workshop.rb
@@ -14,7 +14,21 @@ module Workshop
     [:action]
   end
 
-  def play
-    # Gain a card costing up to 4
+  def play(game, clone=false)
+    @play_thread = Thread.new {
+      available_cards = game.cards_costing_less_than(5).select(&:available?)
+      if available_cards.count == 0
+        @log_updater.custom_message(nil, 'But there are no available cards to gain')
+      else
+        action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, available_cards, 'Choose a card to gain:', 1, 1)
+        TurnActionHandler.process_player_response(game, game.current_player, action, self)
+      end
+      ActiveRecord::Base.clear_active_connections!
+    }
+  end
+
+  def process_action(game, game_player, action)
+    CardGainer.new(game, game_player, action.response).gain_card('discard')
+    TurnActionHandler.refresh_game_area(game, game_player.player)
   end
 end
