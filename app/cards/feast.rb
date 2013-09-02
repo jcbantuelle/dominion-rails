43ffb3e2475de4e4c7eda26eef5630595e42cb50
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-02 16:03:44 -0500

    Implement Feast

--- app/cards/feast.rb
+++ app/cards/feast.rb
@@ -14,8 +14,24 @@ module Feast
     [:action]
   end
 
-  def play
-    # Trash Card
-    # Gain Card costing up to 5
+  def play(game, clone=false)
+    @play_thread = Thread.new {
+      feast = game.current_player.find_card_in_play('feast')
+      CardTrasher.new(game.current_player, [feast]).trash
+      available_cards = game.cards_costing_less_than(6).select(&:available?)
+      if available_cards.count == 0
+        @log_updater.custom_message(nil, 'But there are no available cards to gain')
+      else
+        action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, available_cards, 'Choose a card to gain:', 1, 1)
+        TurnActionHandler.process_player_response(game, game.current_player, action, self)
+      end
+      ActiveRecord::Base.clear_active_connections!
+    }
+  end
+
+  def process_action(game, game_player, action)
+    CardGainer.new(game, game_player, action.response).gain_card('discard')
+    game.reload
+    TurnActionHandler.refresh_game_area(game, game_player.player)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -20,8 +20,8 @@ cards = %w(curse)
 create_cards(cards)
 
 # Base set
-cards = %w(village woodcutter gardens smithy council_room festival laboratory market witch adventurer moneylender chapel cellar bureaucrat militia thief throne_room moat chancellor library workshop)
-# feast remodel spy mine
+cards = %w(village woodcutter gardens smithy council_room festival laboratory market witch adventurer moneylender chapel cellar bureaucrat militia thief throne_room moat chancellor library workshop feast)
+# remodel spy mine
 create_cards(cards, true, false, false, 'base')
 
 # Intrigue
