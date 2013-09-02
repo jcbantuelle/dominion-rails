31548b1cb3114e64e79a643a06d691079cb1f380
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-02 11:47:40 -0500

    Clean Up Seed File

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,5 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
 
 def create_cards(cards, kingdom=false, treasure=false, victory=false, set=nil)
   cards.each do |c|
