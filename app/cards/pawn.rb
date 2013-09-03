3319a32850b9bc382ee5f5da10b9684d6f77a26e
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-02 19:25:04 -0500

    Implement Pawn

---
+++ app/cards/pawn.rb
@@ -0,0 +1,51 @@
+module Pawn
+
+  def starting_count(game)
+    10
+  end
+
+  def cost(game)
+    {
+      coin: 2
+    }
+  end
+
+  def type
+    [:action]
+  end
+
+  def play(game, clone=false)
+    @play_thread = Thread.new {
+      options = [
+        { text: '+1 card', value: 'card' },
+        { text: '+1 action', value: 'action' },
+        { text: '+1 buy', value: 'buy' },
+        { text: '+$1', value: 'coin' },
+      ]
+      action = TurnActionHandler.send_choose_text_prompt(game, game.current_player, options, 'Choose two:', 2, 2)
+      TurnActionHandler.process_player_response(game, game.current_player, action, self)
+      ActiveRecord::Base.clear_active_connections!
+    }
+  end
+
+  def process_action(game, game_player, action)
+    choices = []
+    action.response.split.each do |choice|
+      if choice == 'card'
+        CardDrawer.new(game_player).draw(1)
+      elsif choice == 'action'
+        game.current_turn.add_actions(1)
+        choices << '+1 action'
+      elsif choice == 'buy'
+        game.current_turn.add_buys(1)
+        choices << '+1 buy'
+      elsif choice == 'coin'
+        game.current_turn.add_coins(1)
+        choices << '+$1'
+      end
+    end
+    @log_updater.get_from_card(game.current_player, choices.join(' and '))
+    TurnActionHandler.refresh_game_area(game, game_player.player)
+  end
+
+end
