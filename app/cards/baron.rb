e83695ce1354643673858be0b4d4667d4ffc8b4f
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-08 09:14:03 -0500

    Implement Baron

---
+++ app/cards/baron.rb
@@ -0,0 +1,56 @@
+module Baron
+
+  def starting_count(game)
+    10
+  end
+
+  def cost(game)
+    {
+      coin: 4
+    }
+  end
+
+  def type
+    [:action]
+  end
+
+  def play(game, clone=false)
+    game.current_turn.add_buys(1)
+    @log_updater.get_from_card(game.current_player, '+1 buy')
+
+    estate = game.current_player.find_card_in_hand('estate')
+    if estate.nil?
+      game_estate = GameCard.by_game_id_and_card_name(game.id, 'estate').first
+      CardGainer.new(game, game.current_player, game_estate.id).gain_card('discard')
+    else
+      @play_thread = Thread.new {
+        discard_estate(game, game.current_player)
+        TurnActionHandler.refresh_game_area(game, game.current_player.player)
+        ActiveRecord::Base.clear_active_connections!
+      }
+    end
+  end
+
+  def discard_estate(game, game_player)
+    options = [
+      { text: 'Yes', value: 'yes' },
+      { text: 'No', value: 'no' }
+    ]
+    action = TurnActionHandler.send_choose_text_prompt(game, game.current_player, options, "Discard an estate?".html_safe, 1, 1)
+    TurnActionHandler.process_player_response(game, game_player, action, self)
+  end
+
+  def process_action(game, game_player, action)
+    if action.response == 'yes'
+      estate = game_player.find_card_in_hand('estate')
+      estate.discard
+      @log_updater.discard(game_player, [estate])
+      game.current_turn.add_coins(4)
+      @log_updater.get_from_card(game_player, '+$4')
+    else
+      game_estate = GameCard.by_game_id_and_card_name(game.id, 'estate').first
+      CardGainer.new(game, game_player, game_estate.id).gain_card('discard')
+    end
+  end
+
+end
