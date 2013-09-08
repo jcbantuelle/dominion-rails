34c9ef2bd91d37326202bb665f0ebee3e3b36515
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-08 09:24:49 -0500

    Implement Ironworks

---
+++ app/cards/ironwork.rb
@@ -0,0 +1,49 @@
+module Ironwork
+
+  def starting_count(game)
+    10
+  end
+
+  def cost(game)
+    {
+      coin: 4
+    }
+  end
+
+  def type
+    [:action]
+  end
+
+  def play(game, clone=false)
+    @play_thread = Thread.new {
+      available_cards = game.cards_costing_less_than(5).select(&:available?).select(&:supply?)
+      if available_cards.count == 0
+        @log_updater.custom_message(nil, 'But there are no available cards to gain')
+      else
+        action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, available_cards, 'Choose a card to gain:', 1, 1)
+        TurnActionHandler.process_player_response(game, game.current_player, action, self)
+      end
+      ActiveRecord::Base.clear_active_connections!
+    }
+  end
+
+  def process_action(game, game_player, action)
+    chosen_card = GameCard.find action.response
+    CardGainer.new(game, game_player, action.response).gain_card('discard')
+    gain = []
+    if chosen_card.card.action_card?
+      game.current_turn.add_actions(1)
+      gain << '+1 action'
+    end
+    if chosen_card.card.treasure_card?
+      game.current_turn.add_coins(1)
+      gain << '+$1'
+    end
+    if chosen_card.card.victory_card?
+      CardDrawer.new(game.current_player).draw(1)
+    end
+    LogUpdater.new(game).get_from_card(game.current_player, gain.join(', ')) unless gain.empty?
+    game.reload
+    TurnActionHandler.refresh_game_area(game, game_player.player)
+  end
+end
