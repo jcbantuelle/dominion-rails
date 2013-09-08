08279048c590b168c6609006a6632c489d905fa2
Justin Bantuelle <justin@mudbugmedia.com>
2013-09-08 10:19:47 -0500

    Implement Nobles

---
+++ app/cards/noble.rb
@@ -0,0 +1,47 @@
+module Noble
+
+  def starting_count(game)
+    victory_card_count(game)
+  end
+
+  def cost(game)
+    {
+      coin: 6
+    }
+  end
+
+  def type
+    [:action, :victory]
+  end
+
+  def value(deck)
+    2
+  end
+
+  def play(game, clone=false)
+    @play_thread = Thread.new {
+      options = [
+        { text: '+3 cards', value: 'card' },
+        { text: '+2 action', value: 'action' }
+      ]
+      action = TurnActionHandler.send_choose_text_prompt(game, game.current_player, options, 'Choose one:', 1, 1)
+      TurnActionHandler.process_player_response(game, game.current_player, action, self)
+      ActiveRecord::Base.clear_active_connections!
+    }
+  end
+
+  def process_action(game, game_player, action)
+    if action.response == 'card'
+      CardDrawer.new(game.current_player).draw(3)
+    elsif action.response == 'action'
+      game.current_turn.add_actions(2)
+      @log_updater.get_from_card(game.current_player, '+2 actions')
+    end
+    TurnActionHandler.refresh_game_area(game, game_player.player)
+  end
+
+  def results(deck)
+    card_html
+  end
+
+end
