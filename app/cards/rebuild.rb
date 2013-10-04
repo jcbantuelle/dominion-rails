3ded653bda63d8e25756708968708f524790c934
Justin Bantuelle <justin@mudbugmedia.com>
2013-10-04 18:59:54 -0500

    Implement Rebuild

---
+++ app/cards/rebuild.rb
@@ -0,0 +1,98 @@
+module Rebuild
+
+  def starting_count(game)
+    10
+  end
+
+  def cost(game, turn)
+    {
+      coin: 5
+    }
+  end
+
+  def type
+    [:action]
+  end
+
+  def play(game, clone=false)
+    game.current_turn.add_actions(1)
+    @log_updater.get_from_card(game.current_player, '+1 action')
+
+    if game.current_player.deck.count > 0 || game.current_player.discard.count > 0
+      @play_thread = Thread.new {
+        ActiveRecord::Base.connection_pool.with_connection do
+          name_card(game)
+          ActiveRecord::Base.connection.clear_query_cache
+          TurnActionHandler.refresh_game_area(game, game.current_player.player)
+        end
+      }
+    else
+      @log_updater.custom_message(game.current_player, 'no cards to reveal', 'has')
+    end
+  end
+
+  def name_card(game)
+    cards = game.game_cards
+    action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, cards, 'Choose a card to name:', 1, 1, 'name')
+    TurnActionHandler.process_player_response(game, game.current_player, action, self)
+  end
+
+  def process_action(game, game_player, action)
+    if action.action == 'name'
+      @named_card = GameCard.find(action.response)
+      LogUpdater.new(game).custom_message(game_player, "#{@named_card.card.card_html}".html_safe, 'name')
+      reveal(game, game_player)
+      rebuild_trashed_card(game, game_player) unless @trashed_card.nil?
+    elsif action.action == 'gain'
+      card = GameCard.find(action.response)
+      CardGainer.new(game, game_player, card.name).gain_card('discard')
+    end
+  end
+
+  private
+
+  def reveal(game, game_player)
+    @revealed = []
+    @trashed_card = nil
+    reveal_cards(game, game_player)
+    @log_updater.reveal(game_player, @revealed, 'deck')
+    discard_revealed(game)
+  end
+
+  def rebuild_trashed_card(game, game_player)
+    trashed_card_cost = @trashed_card.calculated_cost(game, game.current_turn)
+    CardTrasher.new(game_player, [@trashed_card]).trash
+    available_cards = game.cards_costing_less_than(trashed_card_cost[:coin]+4, trashed_card_cost[:potion])
+    available_cards = available_cards.select(&:victory_card?)
+    if available_cards.count == 0
+      @log_updater.custom_message(nil, 'But there are no available cards to gain')
+    elsif available_cards.count == 1
+      CardGainer.new(game, game_player, available_cards.first.name).gain_card('discard')
+    else
+      action = TurnActionHandler.send_choose_cards_prompt(game, game_player, available_cards, 'Choose a victory card to gain:', 1, 1, 'gain')
+      TurnActionHandler.process_player_response(game, game_player, action, self)
+    end
+  end
+
+  def process_revealed_card(card)
+    if valid_card?(card)
+      @trashed_card = card
+    else
+      card.update_attribute :state, 'revealed'
+    end
+    valid_card?(card)
+  end
+
+  def discard_revealed(game)
+    game.current_player.discard_revealed
+  end
+
+  def reveal_finished?(game, player)
+    @trashed_card.present? || player.discard.count == 0
+  end
+
+  def valid_card?(card)
+    card.victory? && card.name != @named_card.name
+  end
+
+end
