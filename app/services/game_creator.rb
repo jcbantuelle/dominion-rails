84115f71b8c64fcbcf5c2becfb25e68d41ac42c7
Justin Bantuelle <justin@mudbugmedia.com>
2013-11-27 20:30:03 -0600

    Fix Each With Index Bug in Game Creator

--- app/services/game_creator.rb
+++ app/services/game_creator.rb
@@ -95,13 +95,13 @@ class GameCreator
     %w(abandoned_mine ruined_library ruined_market ruined_village survivors).each do |card_name|
       ruins += ([Card.by_name(card_name)] * 10)
     end
-    ruins.shuffle.take(starting_count).each do |ruin, index|
+    ruins.shuffle.take(starting_count).each_with_index do |ruin, index|
       MixedGameCard.create(game_card: ruins_card, card: ruin, card_order: index, card_type: 'ruins')
     end
   end
 
   def add_knights(knights_card)
-    %w(dame_anna dame_josephine dame_molly dame_natalie dame_sylvia sir_martin sir_bailey sir_destry sir_michael sir_vander).shuffle.each do |card_name, index|
+    %w(dame_anna dame_josephine dame_molly dame_natalie dame_sylvia sir_martin sir_bailey sir_destry sir_michael sir_vander).shuffle.each_with_index do |card_name, index|
       knight = Card.by_name(card_name)
       MixedGameCard.create(game_card: knights_card, card: knight, card_order: index, card_type: 'knights')
     end
