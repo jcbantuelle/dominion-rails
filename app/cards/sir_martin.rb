2bba55bebf2c3e91c4ddf8806f24e5654eb3ed30
Justin Bantuelle <justin@mudbugmedia.com>
2013-11-28 08:41:28 -0600

    Refactor Knights for Inheritance

--- app/cards/sir_martin.rb
+++ app/cards/sir_martin.rb
@@ -1,4 +1,4 @@
-class SirMartin < Card
+class SirMartin < Knight
 
   def cost(game, turn)
     {
@@ -6,69 +6,9 @@ class SirMartin < Card
     }
   end
 
-  def type
-    [:action, :attack, :knight]
-  end
-
   def play(game, clone=false)
     game.current_turn.add_buys(2)
     @log_updater.get_from_card(game.current_player, '+2 buys')
-  end
-
-  def attack(game, players)
-    @attack_thread = Thread.new {
-      ActiveRecord::Base.connection_pool.with_connection do
-        players.each do |player|
-          reveal(game, player)
-          trash_card(game, player)
-          revealed_cards = player.player_cards.revealed
-          CardDiscarder.new(player, revealed_cards).discard
-          TurnActionHandler.wait_for_response(game)
-        end
-      end
-    }
-  end
-
-  def reveal(game, player)
-    @revealed = []
-    reveal_cards(game, player)
-  end
-
-  def process_revealed_card(card)
-    card.update_attribute :state, 'revealed'
-    @revealed.count == 2
-  end
-
-  def reveal_finished?(game, player)
-    @revealed.count == 2 || player.discard.count == 0
-  end
-
-  def trash_card(game, player)
-    if @revealed.count == 0
-      @log_updater.custom_message(nil, 'But there are no cards in deck')
-    else
-      @log_updater.reveal(player, @revealed, 'deck', false)
-      available_cards = @revealed.select{ |card|
-        cost = card.calculated_cost(game, game.current_turn)[:coin]
-        cost > 2 && cost < 7
-      }
-      if available_cards.count == 1
-        trashed_card = available_cards.first
-        CardTrasher.new(player, available_cards).trash
-        trash_self(game) if trashed_card.knight?
-      elsif available_cards.count > 1
-        action = TurnActionHandler.send_choose_cards_prompt(game, player, available_cards, 'Choose which card to trash:', 1, 1, 'trash')
-        TurnActionHandler.process_player_response(game, player, action, self)
-      end
-    end
-  end
-
-  def process_action(game, game_player, action)
-    if action.action == 'trash'
-      card = PlayerCard.find action.response
-      CardTrasher.new(game_player, [card]).trash
-      trash_self(game) if card.knight?
-    end
   end
 
   def trash_self(game)
--- app/cards/sir_michael.rb
+++ app/cards/sir_michael.rb
@@ -1,14 +1,4 @@
-class SirMichael < Card
-
-  def cost(game, turn)
-    {
-      coin: 5
-    }
-  end
-
-  def type
-    [:action, :attack, :knight]
-  end
+class SirMichael < Knight
 
   def play(game, clone=false)
   end
@@ -18,11 +8,7 @@ class SirMichael < Card
       ActiveRecord::Base.connection_pool.with_connection do
         players.each do |player|
           discard_cards(game, player)
-          reveal(game, player)
-          trash_card(game, player)
-          revealed_cards = player.player_cards.revealed
-          CardDiscarder.new(player, revealed_cards).discard
-          TurnActionHandler.wait_for_response(game)
+          knight_attack(game, player)
         end
       end
     }
@@ -39,45 +25,9 @@ class SirMichael < Card
     end
   end
 
-  def reveal(game, player)
-    @revealed = []
-    reveal_cards(game, player)
-  end
-
-  def process_revealed_card(card)
-    card.update_attribute :state, 'revealed'
-    @revealed.count == 2
-  end
-
-  def reveal_finished?(game, player)
-    @revealed.count == 2 || player.discard.count == 0
-  end
-
-  def trash_card(game, player)
-    if @revealed.count == 0
-      @log_updater.custom_message(nil, 'But there are no cards in deck')
-    else
-      @log_updater.reveal(player, @revealed, 'deck', false)
-      available_cards = @revealed.select{ |card|
-        cost = card.calculated_cost(game, game.current_turn)[:coin]
-        cost > 2 && cost < 7
-      }
-      if available_cards.count == 1
-        trashed_card = available_cards.first
-        CardTrasher.new(player, available_cards).trash
-        trash_self(game) if trashed_card.knight?
-      elsif available_cards.count > 1
-        action = TurnActionHandler.send_choose_cards_prompt(game, player, available_cards, 'Choose which card to trash:', 1, 1, 'trash')
-        TurnActionHandler.process_player_response(game, player, action, self)
-      end
-    end
-  end
-
   def process_action(game, game_player, action)
     if action.action == 'trash'
-      card = PlayerCard.find action.response
-      CardTrasher.new(game_player, [card]).trash
-      trash_self(game) if card.knight?
+      knight_trash(game, game_player, action)
     elsif action.action == 'discard'
       discarded_cards = PlayerCard.where(id: action.response.split)
       CardDiscarder.new(game_player, discarded_cards).discard('hand')
