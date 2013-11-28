2b07419ccd6c4ccd3827cc46d90643d1744a2d2b
Justin Bantuelle <justin@mudbugmedia.com>
2013-11-28 08:06:22 -0600

    Refactor Turn Changer

--- app/services/turn_changer.rb
+++ app/services/turn_changer.rb
@@ -36,28 +36,49 @@ class TurnChanger
 
   def clean_up
     resolve_schemes if @game.current_turn.schemes > 0
+    resolve_discard_reactions
+    discard_from_play
+    draw_new_hand
+    revert_band_of_misfits
+  end
+
+  def resolve_discard_reactions
     @game.current_player.in_play.each do |in_play_card|
       in_play_card.card.discard_reaction(@game, @game.current_player, :cleanup) if in_play_card.state != 'duration' && in_play_card.card.respond_to?(:discard_reaction)
     end
+  end
+
+  def discard_from_play
     @game.current_player.player_cards.where(state: %w[hand play]).update_all(state: 'discard')
+  end
+
+  def draw_new_hand
     draw_count = @outpost ? 3 : 5
     CardDrawer.new(@game.current_player).draw(draw_count, false)
-    revert_band_of_misfits
   end
 
   def resolve_schemes
     @game.current_turn.schemes.times do |i|
-      valid_cards = @game.current_player.in_play_without_duration.select(&:action?)
-      action = TurnActionHandler.send_choose_cards_prompt(@game, @game.current_player, valid_cards, "You may choose a card to put on deck:", 1)
-      TurnActionHandler.wait_for_response(@game)
-      action = TurnAction.find_uncached action.id
-      unless action.response.empty?
-        card = PlayerCard.find action.response
-        @game.current_player.put_card_on_deck card
-        LogUpdater.new(@game).put(@game.current_player, [card], 'deck', false)
-      end
+      action = scheme_player_prompt
+      process_scheme_response(action) unless action.response.empty?
       action.destroy
     end
+  end
+
+  def eligible_scheme_cards
+    @game.current_player.in_play_without_duration.select(&:action?)
+  end
+
+  def scheme_player_prompt
+    action = TurnActionHandler.send_choose_cards_prompt(@game, @game.current_player, eligible_scheme_cards, "You may choose a card to put on deck:", 1)
+    TurnActionHandler.wait_for_response(@game)
+    TurnAction.find_uncached action.id
+  end
+
+  def process_scheme_response(action)
+    card = PlayerCard.find action.response
+    @game.current_player.put_card_on_deck card
+    LogUpdater.new(@game).put(@game.current_player, [card], 'deck', false)
   end
 
   def set_game_turn
