d0d2fd7ae618c1e10840952388a4f94b7285392e
Justin Bantuelle <justin@mudbugmedia.com>
2013-11-28 10:38:29 -0600

    Implement Tunnel

---
+++ app/cards/tunnel.rb
@@ -0,0 +1,41 @@
+class Tunnel < Card
+
+  def starting_count(game)
+    victory_card_count(game)
+  end
+
+  def cost(game, turn)
+    {
+      coin: 3
+    }
+  end
+
+  def type
+    [:victory, :reaction]
+  end
+
+  def value(deck)
+    2
+  end
+
+  def results(player)
+    card_html
+  end
+
+  def discard_reaction(game, game_player, event)
+    unless event == :cleanup
+      options = [
+        { text: 'Yes', value: 'yes' },
+        { text: 'No', value: 'no' }
+      ]
+      action = TurnActionHandler.send_choose_text_prompt(game, game_player, options, "Reveal #{card_html}?".html_safe, 1, 1)
+      TurnActionHandler.process_player_response(game, game_player, action, self)
+      ActiveRecord::Base.connection.clear_query_cache
+      TurnActionHandler.refresh_game_area(game, game_player.player)
+    end
+  end
+
+  def process_action(game, game_player, action)
+    CardGainer.new(game, game_player, 'gold').gain_card('discard') if action.response == 'yes'
+  end
+end
--- app/services/card_discarder.rb
+++ app/services/card_discarder.rb
@@ -10,6 +10,7 @@ class CardDiscarder
     discarded_cards = []
     @cards.each do |player_card|
       player_card.update state: 'discard'
+      player_card.card.discard_reaction(@game, @player, :discard) if player_card.card.respond_to?(:discard_reaction)
     end
     LogUpdater.new(@player.game).discard(@player, @cards, source)
     discarded_cards
