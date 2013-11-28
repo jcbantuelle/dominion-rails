b8651cf5e116c891aaf57de20af358cb585d7fb9
Justin Bantuelle <justin@mudbugmedia.com>
2013-11-28 10:12:44 -0600

    Remove Copypasta Commented Code

--- app/services/card_discarder.rb
+++ app/services/card_discarder.rb
@@ -12,15 +12,6 @@ class CardDiscarder
       player_card.update state: 'discard'
     end
     LogUpdater.new(@player.game).discard(@player, @cards, source)
-    # @cards.each do |player_card|
-    #   market_square = @player.find_card_in_hand('market_square')
-    #   unless market_square.nil?
-    #     reaction_card = market_square.card
-    #     reaction_card.reaction(@game, @player)
-    #     TurnActionHandler.wait_for_card(reaction_card)
-    #   end
-    #   player_card.card.trash_reaction(@game, @player) if player_card.card.respond_to?(:trash_reaction)
-    # end
     discarded_cards
   end
 
