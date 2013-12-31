79b477bcf6dcba1b5697197c174350715f11d09b
Justin Bantuelle <justin@mudbugmedia.com>
2013-12-31 08:49:21 -0600

    Fix King's Court Bug

--- app/cards/kings_court.rb
+++ app/cards/kings_court.rb
@@ -23,7 +23,7 @@ class KingsCourt < Card
   end
 
   def process_action(game, game_player, action)
-    play_card_multiple_times(game, game_player, PlayerCard.find(action.response), 3)
+    play_card_multiple_times(game, game_player, PlayerCard.find(action.response), 3) unless action.response.empty?
   end
 
   private
@@ -32,10 +32,8 @@ class KingsCourt < Card
     actions = game.current_player.hand.select(&:action?)
     if actions.count == 0
       @log_updater.custom_message(game.current_player, 'no actions to play', 'have')
-    elsif actions.count == 1
-      play_card_multiple_times(game, game.current_player, actions.first, 3)
     else
-      action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, actions, 'Choose an action to play three times:', 1, 1)
+      action = TurnActionHandler.send_choose_cards_prompt(game, game.current_player, actions, 'You may choose an action to play three times:', 1, 0)
       TurnActionHandler.process_player_response(game, game.current_player, action, self)
     end
   end
