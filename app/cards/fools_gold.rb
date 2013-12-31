d70a2462ddbd0bad7eb5797fcd33f0f46b60937b
Justin Bantuelle <justin@mudbugmedia.com>
2013-12-31 10:26:41 -0600

    Improve Fool's Gold Prompt

--- app/cards/fools_gold.rb
+++ app/cards/fools_gold.rb
@@ -29,7 +29,7 @@ class FoolsGold < Card
             { text: 'Yes', value: 'yes' },
             { text: 'No', value: 'no' }
           ]
-          action = TurnActionHandler.send_choose_text_prompt(game, game_player, options, 'Trash Fools Gold?'.html_safe, 1, 1)
+          action = TurnActionHandler.send_choose_text_prompt(game, game_player, options, "Trash #{card_html}?".html_safe, 1, 1)
           TurnActionHandler.process_player_response(game, game_player, action, self)
           ActiveRecord::Base.connection.clear_query_cache
           TurnActionHandler.refresh_game_area(game, game_player.player)
